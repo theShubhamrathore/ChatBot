18ec75a9d9979c8dd210e046c1f638e4184ddb8e
Shubham Rathore <55997207+theShubhamrathore@users.noreply.github.com>
2023-01-28 18:04:07 +0530

    ChatBot

---
+++ app.py
@@ -0,0 +1,57 @@
+import os
+import openai
+import gradio as gr
+
+#if you have OpenAI API key as an environment variable, enable the below
+#openai.api_key = os.getenv("OPENAI_API_KEY")
+
+#if you have OpenAI API key as a string, enable the below
+openai.api_key = "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"
+
+start_sequence = "\nAI:"
+restart_sequence = "\nHuman: "
+
+prompt = "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: "
+
+def openai_create(prompt):
+
+    response = openai.Completion.create(
+       # share =True;
+    model="text-davinci-003",
+    prompt=prompt,
+    temperature=0.9,
+    max_tokens=150,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0.6,
+    stop=[" Human:", " AI:"]
+    )
+
+    return response.choices[0].text
+
+
+
+def chatgpt_clone(input, history):
+    history = history or []
+    s = list(sum(history, ()))
+    s.append(input)
+    inp = ' '.join(s)
+    output = openai_create(inp)
+    history.append((input, output))
+    return history, history
+
+
+block = gr.Blocks()
+
+
+with block:
+    gr.Markdown("""<h1><center>This is ChatGPT Bot create by Shubham Rathore</center></h1>
+    
+    """)
+    chatbot = gr.Chatbot()
+    message = gr.Textbox(placeholder=prompt)
+    state = gr.State()
+    submit = gr.Button("SUBMIT")
+    submit.click(chatgpt_clone, inputs=[message, state], outputs=[chatbot, state])
+
+block.launch(debug = True)
